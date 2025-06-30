5e684d9b2f366df518fa05e9588bed595eda6716
Debkumar1234 <debkumarmallick2005@gmail.com>
2025-06-30 13:51:18 +0530

    Modify index.ejs for home page

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
+
+
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
